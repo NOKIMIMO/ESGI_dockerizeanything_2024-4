d84bcb487a8f176443fd9f4ed48feac90464e87c
Victoires <berdrin.tristan@gmail.com>
2024-02-20 14:41:46 +0100

    Modification HealthCheck
    Modification du HealthCheck

--- blog/db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-service postgresql start
-service postgresql status
-psql -c "CREATE ROLE user1 WITH LOGIN PASSWORD 'user1';" && \
-    createdb -O user1 myapp && \
-    psql -d myapp -c "ALTER ROLE user1 CREATEDB;"
-createdb -U user1 myapp
